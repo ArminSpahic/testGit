438efed74dd3bab72c6019a2ac341a3b4df66a73
Armin Spahic <ArminSpahic@github.com>
2020-10-13 12:24:51 +0200

    first commit

---
+++ testGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by Armin Spahic on 13/10/2020.
+//  Copyright © 2020 Armin Spahic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
